4b8fc07f324156e5fe4ac4a2c0633854ced0e9d6
justinlyo <jlyogky@kent.edu>
2025-10-22 14:55:09 -0400

    Add logging/error handling to db endpoints

--- backend/src/app/main.py
+++ backend/src/app/main.py
@@ -3,6 +3,7 @@ from fastapi import FastAPI, APIRouter, Depends
 from fastapi.staticfiles import StaticFiles
 from sqlalchemy import text
 from sqlalchemy.future import select
+from sqlalchemy.exc import SQLAlchemyError
 import logging
 from .db import get_db
 from .models import Book
@@ -31,15 +32,25 @@ async def test():
 
 @app.get("/db-test")
 async def db_test(db=Depends(get_db)):
-    result = await db.execute(text("SELECT NOW();"))
-    current_time = result.scalar_one()
-    return {"database_time": str(current_time)}
+    try:
+        result = await db.execute(text("SELECT NOW();"))
+        current_time = result.scalar_one()
+        return {"database_time": str(current_time)}
+    except SQLAlchemyError as e:
+        logging.error("Failed db_test (SQLAlchemyError): %s", e)
+    except Exception as e:
+        logging.error(f"Failed db_test %r", e)
 
 @app.get("/books")
 async def get_books(db=Depends(get_db)):
-    result = await db.execute(select(Book))
-    books = result.scalars().all()
-    return books
+    try:
+        result = await db.execute(select(Book))
+        books = result.scalars().all()
+        return books
+    except SQLAlchemyError as e:
+        logging.error("Failed get_books (SQLAlchemyError): %s", e)
+    except Exception as e:
+        logging.error(f"Failed get_books %r", e)
 
 app.mount("/", StaticFiles(directory=str(FRONTEND_DIR), html=True), name="frontend")
 
