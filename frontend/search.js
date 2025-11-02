efe16cd956a9f9906ab1262a3b605c33910d71aa
Alex Michelbrink <alexmichelbrink26@gmail.com>
2025-11-02 13:37:20 -0500

    added more functionality to browse page and aligned search boxes throughout website. filters still need to be added

--- frontend/search.js
+++ frontend/search.js
@@ -1,15 +1,15 @@
 document.addEventListener("DOMContentLoaded", () => {
     const searchInput = document.getElementById("searchInput");
-
     if (!searchInput) return;
 
-    searchInput.addEventListener("keydown", (event) => {
-        if (event.key === "Enter") {
-            const query = searchInput.value.trim();
-            if (!query) return;
+    const form = searchInput.closest("form");
 
-            // Redirect to search.html with query parameter
-            window.location.href = `/subpages/browse.html?search=${encodeURIComponent(query)}`;
-        }
+    form.addEventListener("submit", (event) => {
+        event.preventDefault();
+        const query = searchInput.value.trim();
+        if (!query) return;
+
+        // Redirect to search.html with query parameter
+        window.location.href = `/subpages/browse.html?search=${encodeURIComponent(query)}&page=1`;
     });
 });
